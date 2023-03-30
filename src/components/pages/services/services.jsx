f4612c241b694670df61bb1856552a30cad9d255
OcttaDev <127272659+OcttaDev@users.noreply.github.com>
2023-03-30 19:05:54 -0300

    responsive update

--- src/components/pages/services/services.jsx
+++ src/components/pages/services/services.jsx
@@ -19,7 +19,7 @@ function Services(){
     return(
         <div className="flex flex-col justify-center items-center gap-10 text-white my-40 responsive" id="services">
             <h1 className="text-3xl">Serviços</h1>
-            <div className="flex gap-3 flex-wrap responsive" data-aos="flip-left">
+            <div className="flex gap-3 flex-wrap responsive">
                 <Service icon={<IoCodeOutline size={30}/>} title="Criação de Sites" />
                 <Service icon={<IoPhonePortraitOutline size={30}/>} secundIcon={<IoDesktopOutline size={30}/>} title="Sites responsivos"/>
                 <Service icon={<IoPhonePortraitOutline size={30}/>} title="Desenvolvimento mobile (em formação)" />
